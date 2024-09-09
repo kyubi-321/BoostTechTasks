38f430ae430cae03d6caa0e66bdf640e85567b59
kyubi-321 <ankitbadhani102@gmail.com>
2024-09-09 10:40:54 +0530

    changes it to index.html

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $experience = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Me - <?php echo $name; ?></title>
-    <link rel="stylesheet" href="about.css"> 
+    <link rel="stylesheet" href="index.css"> 
 </head>
 <body>
 
